904b81b4685cb885288998cea1a04bd0bb9dd2c6
Jarrod <Demonico@users.noreply.github.com>
2018-07-12 12:51:15 -0600

    initial working state

--- client/App.tsx
+++ client/App.tsx
@@ -5,6 +5,6 @@ interface IState {}
 
 export class App extends React.Component<IProps,IState> {
   render() {
-    return <h1>Hello {this.props.name}!</h1>
+    return <h1>Hello World!</h1>
   }
 }
--- client/index.tsx
+++ client/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import * as ReactDom from 'react-dom'
 
-import { App } from './components/App'
+import { App } from './App'
 
 
 const root = document.getElementById('root')
