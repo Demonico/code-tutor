29c3308df2d0d279d5d564a19e63ee1cccf5ef03
Jarrod <Demonico@users.noreply.github.com>
2018-07-12 12:36:54 -0600

    added base client files

---
+++ client/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react'
+import * as ReactDom from 'react-dom'
+
+import { App } from './components/App'
+
+
+const root = document.getElementById('root')
+
+ReactDom.render(<App/>, root)
--- server.js
+++ server.js
@@ -2,9 +2,13 @@
 const express = require('express')
 const path = require('path')
 const cookieParser = require('cookie-parser')
+const mongoose = require("mongoose")
 
 // Route Definitions
 const indexRouter = require('./routes/index')
+
+// Port config
+const PORT = process.env.PORT || 3001;
 
 // Initialize Express
 const app = express()
@@ -18,3 +22,13 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Routes Index
 app.use('/', indexRouter)
 
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI ||
+  "mongodb://localhost/codetutor"
+)
+
+// Start the API server
+app.listen(PORT, () =>
+  console.log(`API Server now listening on PORT ${PORT}!`)
+)
