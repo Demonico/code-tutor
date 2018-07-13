a6d23340f39cbe3878a14c95dafdd1af713d0dd7
Jarrod <Demonico@users.noreply.github.com>
2018-07-12 18:34:03 -0600

    converted to use parcel middleware

--- server.js
+++ server.js
@@ -1,34 +1,28 @@
 // Packages
+const Bundler = require('parcel-bundler')
 const express = require('express')
 const path = require('path')
-const cookieParser = require('cookie-parser')
-const mongoose = require("mongoose")
+// Bundler Config=
+const insertionFile = path.join(__dirname, 'client', 'index.html')
+const { devOpts, prodOpts } = require('./parcel-options')
+
+// Initialize a new bundler using a file and options
+const options = process.env.NODE_ENV !== 'production' ? devOpts : prodOpts
+const bundler = new Bundler(insertionFile, {})
 
 // Route Definitions
-const indexRouter = require('./routes/index')
+const indexRouter = require('./routes')
 
 // Port config
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000
 
 // Initialize Express
 const app = express()
 
-// Express Config
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(bundler.middleware())
 
 // Routes Index
-app.use('/', indexRouter)
-
-// Connect to the Mongo DB
-mongoose.connect(
-  process.env.MONGODB_URI ||
-  "mongodb://localhost/codetutor"
-)
+app.use(indexRouter)
 
 // Start the API server
-app.listen(PORT, () =>
-  console.log(`API Server now listening on PORT ${PORT}!`)
-)
+app.listen(PORT, () => console.log(`API Server now listening on PORT ${PORT}!`))
